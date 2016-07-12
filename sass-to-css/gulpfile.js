d3cdfd010785ab04a765adf1c8ffff861fbba999
Michael Norelli <mnorelli360@gmail.com>
2016-07-12 14:00:44 -0700

    adds gulp styles task and scss and css files

---
+++ sass-to-css/gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
